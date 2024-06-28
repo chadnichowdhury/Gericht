a143499e4e8ef877f6da788aac4f4d96e0f3da71
Marimo-103 <m.2091hasan@gmail.com>
2024-06-28 16:41:50 +0600

    yooo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,27 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+
       fontFamily: {
         'common': ["Open Sans", "sans-serif"],
         'sub': ["Cormorant Upright", "serif"],
       },
+
+      backgroundImage: {
+        'image': "url('./Images/Footer-bg.png')",
+      },
+
+      animation: {
+        'rotate': 'spinSlow 10s linear infinite',
+      },
+
+      keyframes: {
+        spinSlow: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        }
+      },
+
     },
   },
   plugins: [],

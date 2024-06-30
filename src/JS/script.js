7f0ef8928fccbb40585e100e6372aa3ae577abeb
Marimo-103 <m.2091hasan@gmail.com>
2024-06-30 16:01:56 +0600

    service half

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,7 +1,13 @@
 
-
-
-
-
-
-
+// service part slider js..........
+$('.banner_slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed:1500,
+    prevArrow:false,
+    nextArrow:false,
+  });
